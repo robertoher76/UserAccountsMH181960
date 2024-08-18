99ebd3209c5f76d7e42f124ee83098187d12dee9
robertoher76 <robertoher76@gmail.com>
2024-08-17 20:13:42 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserAccountsMH181960.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
